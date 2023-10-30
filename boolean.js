5c0b0f4dc02e470bb8ae8d553e83d2621452ba53
George <MY_NAME@example.com>
2023-10-30 18:43:33 +0300

    add: create loop.js file

---
+++ boolean.js
@@ -0,0 +1,8 @@
+const fnum = 5
+const snum = 7
+if (fnum  ) {
+    console.log ( "данное число" + fnum + "больше" + snum )
+
+}else{
+    console.log ("данное число $(fnum) больше $ ')
+}
---
+++ loop.js
@@ -0,0 +1,9 @@
+for (let i=0; i<=11; i++){
+    console.log (`на ${i} итерации число ${i}`)
+}
+
+let count = 1
+for (let i=7894; i<8000; i++){
+    if (i%2 !==0)
+    console.log (`на ${i} итерации число ${i}`)
+}
